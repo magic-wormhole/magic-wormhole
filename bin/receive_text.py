d4233f44df42f4850288be0cf0bfda08dba6cce8
Brian Warner <warner@lothar.com>
2015-02-11 01:45:54 -0800

    bin/receive_text: print elapsed time

--- bin/receive_text.py
+++ bin/receive_text.py
@@ -1,13 +1,15 @@
 from __future__ import print_function
-import sys, json
+import time, sys, json
 from wormhole.transcribe import Receiver
 
 APPID = "lothar.com/wormhole/text-xfer"
 
 # we're receiving
+start = time.time()
 code = sys.argv[1]
 blob = b"{}"
 r = Receiver(APPID, blob, code)
 them_bytes = r.get_data()
 them_d = json.loads(them_bytes.decode("utf-8"))
 print(them_d["message"])
+print("elapsed time: %.2f" % (time.time() - start))
