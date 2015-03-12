8dfe4e7b8d9f6e3f52271c41659010086c1a24dd
Brian Warner <warner@lothar.com>
2015-03-12 16:03:14 -0700

    TRANSIT_RELAY: update to new hint format

--- src/wormhole/const.py
+++ src/wormhole/const.py
@@ -2,4 +2,4 @@
 # This is a relay I run on a personal server. If it gets too expensive to
 # run, I'll shut it down.
 RENDEZVOUS_RELAY = "http://relay.petmail.org:8009/relay/"
-TRANSIT_RELAY = "relay.petmail.org,8010"
+TRANSIT_RELAY = "tcp:relay.petmail.org:8010"
