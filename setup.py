b97b7ccd8cf277df7487f1bb5af1645bbd0eda2e
Brian Warner <warner@lothar.com>
2015-09-22 01:03:43 -0700

    switch to spake2==0.3, with the faster symmetric algorithm
    Note that this breaks compatibility of SymmetricWormhole, which was
    previously only used by the Twisted flavor.

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(name="magic-wormhole",
       package_data={"wormhole": ["db-schemas/*.sql"]},
       entry_points={"console_scripts":
                     ["wormhole = wormhole.scripts.runner:entry"]},
-      install_requires=["spake2==0.2", "pynacl", "requests", "argparse"],
+      install_requires=["spake2==0.3", "pynacl", "requests", "argparse"],
       test_suite="wormhole.test",
       cmdclass=commands,
       )
