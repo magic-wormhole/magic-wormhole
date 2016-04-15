db137c26e5cc1f3a8bc219bb429c4f173b1d300b
Brian Warner <warner@lothar.com>
2016-04-15 16:39:05 -0700

    remove blocking.transit

--- src/wormhole/test/test_blocking.py
+++ src/wormhole/test/test_blocking.py
@@ -1,14 +1,11 @@
 from __future__ import print_function
 import json
 from twisted.trial import unittest
-from twisted.internet.defer import gatherResults, succeed, inlineCallbacks
+from twisted.internet.defer import gatherResults, succeed
 from twisted.internet.threads import deferToThread
 from ..blocking.transcribe import (Wormhole, UsageError, ChannelManager,
                                    WrongPasswordError)
 from ..blocking.eventsource import EventSourceFollower
-from ..blocking.transit import (TransitSender, TransitReceiver,
-                                build_sender_handshake,
-                                build_receiver_handshake)
 from .common import ServerBase
 
 APPID = u"appid"
@@ -447,101 +444,3 @@ class EventSourceClient(unittest.TestCase):
                               (u"message", u"three"),
                               (u"e2", u"four"),
                               ])
-
-class Transit(_DoBothMixin, ServerBase, unittest.TestCase):
-    def test_hints(self):
-        r = TransitReceiver(self.transit)
-        hints = r.get_direct_hints()
-        self.assertTrue(len(hints), hints)
-
-    @inlineCallbacks
-    def test_direct_to_receiver(self):
-        s = TransitSender(self.transit)
-        r = TransitReceiver(self.transit)
-        key = b"\x00"*32
-
-        # force the connection to be sender->receiver
-        s.set_transit_key(key)
-        # only use 127.0.0.1
-        hint = u"tcp:127.0.0.1:%d" % r.listener.server_address[1]
-        s.add_their_direct_hints([hint])
-        s.add_their_relay_hints([])
-        r.set_transit_key(key)
-        r.add_their_direct_hints([])
-        r.add_their_relay_hints([])
-
-        # it'd be nice to factor this chunk out with 'yield from', but that
-        # didn't appear until python-3.3, and isn't in py2 at all.
-        (sp, rp) = yield self.doBoth([s.connect], [r.connect])
-        yield deferToThread(sp.send_record, b"01234")
-        rec = yield deferToThread(rp.receive_record)
-        self.assertEqual(rec, b"01234")
-        yield deferToThread(sp.close)
-        yield deferToThread(rp.close)
-
-    @inlineCallbacks
-    def test_direct_to_sender(self):
-        s = TransitSender(self.transit)
-        r = TransitReceiver(self.transit)
-        key = b"\x00"*32
-
-        # force the connection to be receiver->sender
-        s.set_transit_key(key)
-        s.add_their_direct_hints([])
-        s.add_their_relay_hints([])
-        r.set_transit_key(key)
-        hint = u"tcp:127.0.0.1:%d" % s.listener.server_address[1]
-        r.add_their_direct_hints([hint])
-        r.add_their_relay_hints([])
-
-        (sp, rp) = yield self.doBoth([s.connect], [r.connect])
-        yield deferToThread(sp.send_record, b"01234")
-        rec = yield deferToThread(rp.receive_record)
-        self.assertEqual(rec, b"01234")
-        yield deferToThread(sp.close)
-        yield deferToThread(rp.close)
-
-    @inlineCallbacks
-    def test_relay(self):
-        s = TransitSender(self.transit)
-        r = TransitReceiver(self.transit)
-        key = b"\x00"*32
-        # force the connection to use the relay by not revealing direct hints
-        s.set_transit_key(key)
-        s.add_their_direct_hints([])
-        s.add_their_relay_hints(r.get_relay_hints())
-        r.set_transit_key(key)
-        r.add_their_direct_hints([])
-        r.add_their_relay_hints(s.get_relay_hints())
-
-        (sp, rp) = yield self.doBoth([s.connect], [r.connect])
-        yield deferToThread(sp.send_record, b"01234")
-        rec = yield deferToThread(rp.receive_record)
-        self.assertEqual(rec, b"01234")
-        yield deferToThread(sp.close)
-        yield deferToThread(rp.close)
-        # TODO: this may be racy if we don't poll the server to make sure
-        # it's witnessed the first connection closing before querying the DB
-        #import time
-        #yield deferToThread(time.sleep, 1)
-
-        # check the transit relay's DB, make sure it counted the bytes
-        db = self._transit_server._db
-        c = db.execute("SELECT * FROM `usage` WHERE `type`=?", (u"transit",))
-        rows = c.fetchall()
-        self.assertEqual(len(rows), 1)
-        row = rows[0]
-        self.assertEqual(row["result"], u"happy")
-        # Sender first writes relay_handshake and waits for OK, but that's
-        # not counted by the transit server. Then sender writes
-        # sender_handshake and waits for receiver_handshake. Then sender
-        # writes GO and the body. Body is length-prefixed SecretBox, so
-        # includes 4-byte length, 24-byte nonce, and 16-byte MAC.
-        sender_count = (len(build_sender_handshake(b""))+
-                        len(b"go\n")+
-                        4+24+len(b"01234")+16)
-        # Receiver first writes relay_handshake and waits for OK, but that's
-        # not counted. Then receiver writes receiver_handshake and waits for
-        # sender_handshake+GO.
-        receiver_count = len(build_receiver_handshake(b""))
-        self.assertEqual(row["total_bytes"], sender_count+receiver_count)
