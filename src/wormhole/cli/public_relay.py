77661bf94e88dd7c774cf6479991817fead2155a
Brian Warner <warner@lothar.com>
2016-05-24 13:10:45 -0700

    use new relay URL, for new protocol

--- src/wormhole/cli/public_relay.py
+++ src/wormhole/cli/public_relay.py
@@ -1,5 +1,5 @@
 
 # This is a relay I run on a personal server. If it gets too expensive to
 # run, I'll shut it down.
-RENDEZVOUS_RELAY = u"http://wormhole-relay.petmail.org:3000/wormhole-relay/"
-TRANSIT_RELAY = u"tcp:wormhole-transit-relay.petmail.org:3001"
+RENDEZVOUS_RELAY = u"ws://wormhole-relay.petmail.org:4000/"
+TRANSIT_RELAY = u"tcp:wormhole-transit-relay.petmail.org:4001"
