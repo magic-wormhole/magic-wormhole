783cd984f61cacbeceeec21a6d06a21e5e9eac0a
Brian Warner <warner@lothar.com>
2016-05-24 16:03:39 -0700

    add 'coverage' environment for tox

---
+++ src/wormhole/test/run_trial.py
@@ -0,0 +1,16 @@
+
+# This is a tiny helper module, to let "python -m wormhole.test.run_trial
+# ARGS" does the same thing as running "trial ARGS" (unfortunately
+# twisted/scripts/trial.py does not have a '__name__=="__main__"' clause).
+#
+# This makes it easier to run trial under coverage from tox:
+# * "coverage run trial ARGS" is how you'd usually do it
+# * but "trial" must be the one in tox's virtualenv
+# * "coverage run `which trial` ARGS" works from a shell
+# * but tox doesn't use a shell
+# So use:
+#  "coverage run -m wormhole.test.run_trial ARGS"
+
+from twisted.scripts.trial import run
+
+run()
