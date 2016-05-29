ea6619bc46ce92e5a19044e1b18ad1b922b5f974
Brian Warner <warner@lothar.com>
2016-05-28 19:13:44 -0700

    change hostname of public relay
    This is an alias for the same host, so it's not really an incompatible
    change. The new hostname is my personal domain, and seems a bit more
    suitable for this service.

--- src/wormhole/cli/public_relay.py
+++ src/wormhole/cli/public_relay.py
@@ -1,5 +1,5 @@
 
 # This is a relay I run on a personal server. If it gets too expensive to
 # run, I'll shut it down.
-RENDEZVOUS_RELAY = u"ws://wormhole-relay.petmail.org:4000/v1"
-TRANSIT_RELAY = u"tcp:wormhole-transit-relay.petmail.org:4001"
+RENDEZVOUS_RELAY = u"ws://wormhole-relay.lothar.com:4000/v1"
+TRANSIT_RELAY = u"tcp:wormhole-transit.lothar.com:4001"
