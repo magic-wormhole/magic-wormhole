e98a6c501b9aeab6fdc64e6cc4faf078a933b4cc
laharah <laharah22@gmail.com>
2016-06-02 15:09:42 -0700

    added windows extra to setup

--- setup.py
+++ setup.py
@@ -29,7 +29,9 @@ setup(name="magic-wormhole",
                         "autobahn[twisted] >= 0.14.1",
                         "hkdf", "tqdm",
                         ],
-      extras_require={"tor": ["txtorcon", "ipaddress"]},
+      extras_require={"tor": ["txtorcon", "ipaddress"],
+                      "windows": ["pypiwin32"]
+                      },
       test_suite="wormhole.test",
       cmdclass=commands,
       )
