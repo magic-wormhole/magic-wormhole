e6f339548a3e39f1b3141c24801e5c6ed0745a0b
Brian Warner <warner@lothar.com>
2017-05-21 13:42:36 -0700

    port some of old mypy branch forward to current master

---
+++ _version.pyi
@@ -0,0 +1,4 @@
+from typing import Dict, Text, Any
+LONG_VERSION_PY = {} # type: Dict[Text, Text]
+HANDLERS = {} # type: Dict[Text, Dict[Text, Any]]
+def get_versions(): ... # type: () -> Dict[Text, Text]
