85ec9ae00b7493385d5c5d419f2f5f84a83c22ef
cclauss <cclauss@bluewin.ch>
2017-07-19 08:27:00 +0200

    Resolve conflict

--- misc/demo-journal.py
+++ misc/demo-journal.py
@@ -1,5 +1,5 @@
 from __future__ import print_function
-import os, sys, json
+import os, sys, json, contextlib, random
 from twisted.internet import task, defer, endpoints
 from twisted.application import service, internet
 from twisted.web import server, static, resource
