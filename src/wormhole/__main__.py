74da87bec2164299b9fb2f5c2b9e4a174089d1f9
Brian Warner <warner@lothar.com>
2018-11-13 09:59:36 -0800

    change `__main__.py` to use relative imports
    This should fix `python -m wormhole` on py2. Closes #315.

--- src/wormhole/__main__.py
+++ src/wormhole/__main__.py
@@ -1,4 +1,5 @@
-from wormhole.cli import cli
+from __future__ import absolute_import, print_function, unicode_literals
+from .cli import cli
 
 if __name__ != "__main__":
     raise ImportError('this module should not be imported')
