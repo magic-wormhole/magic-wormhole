a12816743fea6a4fcede259bfffbdf78b1da1a40
meejah <meejah@meejah.ca>
2022-08-22 14:54:36 -0600

    fix test

--- src/wormhole/test/test_machines.py
+++ src/wormhole/test/test_machines.py
@@ -1318,7 +1318,7 @@ class Boss(unittest.TestCase):
                      timing.DebugTiming())
         b._T = Dummy("t", events, ITerminator, "close")
         b._S = Dummy("s", events, ISend, "send")
-        b._RC = Dummy("rc", events, IRendezvousConnector, "start")
+        b._RC = Dummy("rc", events, IRendezvousConnector, "start", "_send_bind")
         b._C = Dummy("c", events, ICode, "allocate_code", "input_code",
                      "set_code")
         b._D = Dummy("d", events, IDilator, "got_wormhole_versions", "got_key")
@@ -1338,6 +1338,7 @@ class Boss(unittest.TestCase):
         b.rx_welcome(welcome)
         self.assertEqual(events, [
             ("w.got_welcome", welcome),
+            ("rc._send_bind", ),
         ])
         events[:] = []
 
