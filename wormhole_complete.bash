cfce4d460dba4582c15bb2046783e11a6510cf9b
meejah <meejah@meejah.ca>
2023-09-18 17:35:53 -0600

    Bash tab-completion

---
+++ wormhole_complete.bash
@@ -0,0 +1,29 @@
+_wormhole_completion() {
+    local IFS=$'\n'
+    local response
+
+    response=$(env COMP_WORDS="${COMP_WORDS[*]}" COMP_CWORD=$COMP_CWORD _WORMHOLE_COMPLETE=bash_complete $1)
+
+    for completion in $response; do
+        IFS=',' read type value <<< "$completion"
+
+        if [[ $type == 'dir' ]]; then
+            COMPREPLY=()
+            compopt -o dirnames
+        elif [[ $type == 'file' ]]; then
+            COMPREPLY=()
+            compopt -o default
+        elif [[ $type == 'plain' ]]; then
+            COMPREPLY+=($value)
+        fi
+    done
+
+    return 0
+}
+
+_wormhole_completion_setup() {
+    complete -o nosort -F _wormhole_completion wormhole
+}
+
+_wormhole_completion_setup;
+
