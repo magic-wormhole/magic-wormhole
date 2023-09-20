e77dbd55059487b2874b8c0971e4d9040af3fce1
meejah <meejah@meejah.ca>
2023-09-19 21:58:19 -0600

    fish, zsh completions

---
+++ wormhole_complete.fish
@@ -0,0 +1,18 @@
+function _wormhole_completion;
+    set -l response (env _WORMHOLE_COMPLETE=fish_complete COMP_WORDS=(commandline -cp) COMP_CWORD=(commandline -t) wormhole);
+
+    for completion in $response;
+        set -l metadata (string split "," $completion);
+
+        if test $metadata[1] = "dir";
+            __fish_complete_directories $metadata[2];
+        else if test $metadata[1] = "file";
+            __fish_complete_path $metadata[2];
+        else if test $metadata[1] = "plain";
+            echo $metadata[2];
+        end;
+    end;
+end;
+
+complete --no-files --command wormhole --arguments "(_wormhole_completion)";
+
