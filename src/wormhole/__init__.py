ee424c73a85f2b33aead8553ba7aa37a3eeedc05
meejah <meejah@meejah.ca>
2023-10-24 18:28:59 -0600

    reord

--- src/wormhole/__init__.py
+++ src/wormhole/__init__.py
@@ -1,7 +1,7 @@
 from ._rlcompleter import input_with_completion
 from .wormhole import create, __version__
 
-__all__ = ["create", "input_with_completion", "__version__"]
-
 from . import _version
 __version__ = _version.get_versions()['version']
+
+__all__ = ["create", "input_with_completion", "__version__"]
