a73c196069108c0615bed733ddb2e1293228d8b9
meejah <meejah@meejah.ca>
2024-01-29 18:22:24 -0700

    no more MD

--- docs/conf.py
+++ docs/conf.py
@@ -20,12 +20,6 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
-from recommonmark.parser import CommonMarkParser
-
-source_parsers = {
-    ".md": CommonMarkParser,
-}
-
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
