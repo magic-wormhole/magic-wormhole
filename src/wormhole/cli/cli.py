fc92408855b605dfa18d1fb5ceb103d721206b49
meejah <meejah@meejah.ca>
2024-10-21 17:16:23 -0600

    wrap reactor, no need for Twisted patch

--- src/wormhole/cli/cli.py
+++ src/wormhole/cli/cli.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import time
 start = time.time()
 
@@ -128,6 +129,56 @@ def wormhole(context, dump_timing, transit_helper, relay_url, appid):
     cfg.dump_timing = dump_timing
 
 
+from twisted.internet import tcp
+from functools import wraps
+import socket
+
+class ReusePort(tcp.Port):
+    def createInternetSocket(self, *args, **kw):
+        skt = tcp.Port.createInternetSocket(self, *args, **kw)
+        # REUSE_ADDR already set by twisted
+        if tcp.platformType == "posix" and sys.platform != "cygwin":
+            skt.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+        return skt
+
+
+class ReuseClient(tcp.Client):
+    def createInternetSocket(self, *args, **kw):
+        skt = tcp.Client.createInternetSocket(self, *args, **kw)
+        # copied platform logic from Port, but Twisted issue comments
+        # say REUSEADDR along on Windows is similar-enough to both on
+        # Linux (?)
+        if tcp.platformType == "posix" and sys.platform != "cygwin":
+            skt.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            skt.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+        return skt
+
+
+class ReuseConnector(tcp.Connector):
+    def _makeTransport(self):
+        return ReuseClient(self.host, self.port, self.bindAddress, self, self.reactor)
+
+
+
+def _wrap_reactor(reactor):
+
+    @wraps(reactor.listenTCP)
+    def _listenTCP(port, factory, backlog=50, interface=""):
+        p = ReusePort(port, factory, backlog, interface, reactor)
+        p.startListening()
+        return p
+    reactor.listenTCP = _listenTCP
+
+    @wraps(reactor.connectTCP)
+    def _connectTCP(host, port, factory, timeout=30, bindAddress=None):
+        c = ReuseConnector(host, port, factory, timeout, bindAddress, reactor)
+        c.connect()
+        return c
+    reactor.connectTCP = _connectTCP
+
+    return reactor
+
+
 @inlineCallbacks
 def _dispatch_command(reactor, cfg, command):
     """
@@ -135,6 +186,9 @@ def _dispatch_command(reactor, cfg, command):
     callable) with the Config instance in cfg and interprets any
     errors for the user.
     """
+
+    reactor = _wrap_reactor(reactor)
+
     cfg.timing.add("command dispatch")
     cfg.timing.add(
         "import", when=start, which="top").finish(when=top_import_finish)
