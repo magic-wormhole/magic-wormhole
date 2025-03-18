8a294eaa4fcbdcb4aea5598402dc10dd84a89a21
meejah <meejah@meejah.ca>
2025-03-18 13:50:19 -0600

    convert to pytest (text_xfer_util)

--- src/wormhole/test/test_xfer_util.py
+++ src/wormhole/test/test_xfer_util.py
@@ -5,57 +5,60 @@ from twisted.trial import unittest
 from .. import xfer_util
 from .common import ServerBase
 
+import pytest_twisted
+
 APPID = u"appid"
 
 
-class Xfer(ServerBase, unittest.TestCase):
-    @inlineCallbacks
-    def test_xfer(self):
-        code = u"1-code"
-        data = u"data"
-        d1 = xfer_util.send(reactor, APPID, self.relayurl, data, code)
-        d2 = xfer_util.receive(reactor, APPID, self.relayurl, code)
-        send_result = yield d1
-        receive_result = yield d2
-        self.assertEqual(send_result, None)
-        self.assertEqual(receive_result, data)
+@pytest_twisted.ensureDeferred
+async def test_xfer(mailbox):
+    code = u"1-code"
+    data = u"data"
+    d1 = xfer_util.send(reactor, APPID, mailbox, data, code)
+    d2 = xfer_util.receive(reactor, APPID, mailbox, code)
+    send_result = await d1
+    receive_result = await d2
+    assert send_result is None
+    assert receive_result == data
 
-    @inlineCallbacks
-    def test_on_code(self):
-        code = u"1-code"
-        data = u"data"
-        send_code = []
-        receive_code = []
-        d1 = xfer_util.send(
-            reactor,
-            APPID,
-            self.relayurl,
-            data,
-            code,
-            on_code=send_code.append)
-        d2 = xfer_util.receive(
-            reactor, APPID, self.relayurl, code, on_code=receive_code.append)
-        send_result = yield d1
-        receive_result = yield d2
-        self.assertEqual(send_code, [code])
-        self.assertEqual(receive_code, [code])
-        self.assertEqual(send_result, None)
-        self.assertEqual(receive_result, data)
 
-    @inlineCallbacks
-    def test_make_code(self):
-        data = u"data"
-        got_code = defer.Deferred()
-        d1 = xfer_util.send(
-            reactor,
-            APPID,
-            self.relayurl,
-            data,
-            code=None,
-            on_code=got_code.callback)
-        code = yield got_code
-        d2 = xfer_util.receive(reactor, APPID, self.relayurl, code)
-        send_result = yield d1
-        receive_result = yield d2
-        self.assertEqual(send_result, None)
-        self.assertEqual(receive_result, data)
+@pytest_twisted.ensureDeferred
+async def test_on_code(mailbox):
+    code = u"1-code"
+    data = u"data"
+    send_code = []
+    receive_code = []
+    d1 = xfer_util.send(
+        reactor,
+        APPID,
+        mailbox,
+        data,
+        code,
+        on_code=send_code.append)
+    d2 = xfer_util.receive(
+        reactor, APPID, mailbox, code, on_code=receive_code.append)
+    send_result = await d1
+    receive_result = await d2
+    assert send_code == [code]
+    assert receive_code == [code]
+    assert send_result is None
+    assert receive_result == data
+
+
+@pytest_twisted.ensureDeferred
+async def test_make_code(mailbox):
+    data = u"data"
+    got_code = defer.Deferred()
+    d1 = xfer_util.send(
+        reactor,
+        APPID,
+        mailbox,
+        data,
+        code=None,
+        on_code=got_code.callback)
+    code = await got_code
+    d2 = xfer_util.receive(reactor, APPID, mailbox, code)
+    send_result = await d1
+    receive_result = await d2
+    assert send_result is None
+    assert receive_result == data
