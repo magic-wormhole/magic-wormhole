58af62bbbde60b43b083218059094aaf9b09ad7f
meejah <meejah@meejah.ca>
2025-03-18 13:42:45 -0600

    cleanup, exclude from test with strategy not assume

--- src/wormhole/test/test_xfer_util_pytest.py
+++ src/wormhole/test/test_xfer_util_pytest.py
@@ -14,16 +14,15 @@ from hypothesis import given, assume
 
 
 @given(
-    password=st.text(),
+    password=st.text(
+        alphabet=st.characters(exclude_categories=["Zs"]),
+    ),
     nameplate=st.integers(min_value=1, max_value=2000),
+    data=st.text(),
 )
 @pytest_twisted.ensureDeferred
-async def test_xfer(password, nameplate, mailbox):
-    assume(" " not in password)  # XXX instead don't generate spaces
+async def test_xfer(password, nameplate, data, mailbox):
     code = f"{nameplate}-{password}"
-    print(mailbox, code)
-
-    data = u"data"
     d1 = xfer_util.send(reactor, APPID, mailbox, data, code)
     d2 = xfer_util.receive(reactor, APPID, mailbox, code)
     send_result = await d1
