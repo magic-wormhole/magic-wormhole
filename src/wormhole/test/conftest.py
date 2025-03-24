358a39fe65ff1d03c1e7615dee5832acdb686aca
meejah <meejah@meejah.ca>
2025-03-24 12:50:25 -0600

    transit_relay fixture (untested)

--- src/wormhole/test/conftest.py
+++ src/wormhole/test/conftest.py
@@ -1,11 +1,18 @@
 from twisted.internet import endpoints
+from twisted.internet.protocol import ServerFactory
+from twisted.application.internet import StreamServerEndpointService
 
 from wormhole_mailbox_server.database import create_channel_db, create_usage_db
 from wormhole_mailbox_server.server import make_server
 from wormhole_mailbox_server.web import make_web_server
+from wormhole_transit_relay.transit_server import Transit, TransitConnection
+from wormhole_transit_relay.usage import create_usage_tracker
+
 
 import pytest
 import pytest_twisted
+
+from ..transit import allocate_tcp_port
 
 
 @pytest.fixture(scope="session")
@@ -27,3 +34,18 @@ def mailbox(reactor):
 
     pytest_twisted.blockon(port.stopListening())
 
+
+@pytest.fixture(scope="session")
+def transit_relay(reactor):
+    transitport = allocate_tcp_port()
+    endpoint = f"tcp:{transitport}:interface=127.0.0.1"
+    ep = endpoints.serverFromString(reactor, endpoint)
+    usage = create_usage_tracker(blur_usage=None, log_file=None, usage_db=None)
+    transit_server = ServerFactory()
+    transit_server.protocol = TransitConnection
+    transit_server.log_requests = False
+    transit_server.transit = Transit(usage, reactor.seconds)
+    service = StreamServerEndpointService(ep, transit_server)
+    pytest_twisted.blockon(service.startService())
+    yield endpoint
+    pytest_twisted.blockon(service.stopService())
