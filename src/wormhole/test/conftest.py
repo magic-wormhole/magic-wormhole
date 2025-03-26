b70c166278ffb581fa6b2f750d39b592f762b591
meejah <meejah@meejah.ca>
2025-03-25 20:27:48 -0600

    WIP: try to fix eventual tests

--- src/wormhole/test/conftest.py
+++ src/wormhole/test/conftest.py
@@ -27,3 +27,47 @@ def transit_relay(reactor):
     service.startService()
     yield url
     pytest_twisted.blockon(service.stopService())
+
+
+
+# from kyle altendorf
+# see also https://github.com/pytest-dev/pytest-twisted/issues/4
+import gc
+import twisted.logger
+
+
+class Observer:
+    def __init__(self):
+        self.failures = []
+
+    def __call__(self, event_dict):
+        is_error = event_dict.get('isError')
+        s = 'Unhandled error in Deferred'.casefold()
+        is_unhandled = s in event_dict.get('log_format', '').casefold()
+
+        if is_error and is_unhandled:
+            self.failures.append(event_dict)
+
+    def flush(self, klass):
+        for f in self.failures:
+            print(f, klass)
+
+    def assert_empty(self):
+        assert [] == self.failures
+
+
+@pytest.fixture
+def observe_errors():
+    observer = Observer()
+    twisted.logger.globalLogPublisher.addObserver(observer)
+
+    yield observer
+
+    gc.collect()
+    gc.collect()
+    gc.collect()
+    gc.collect()
+    gc.collect()
+    twisted.logger.globalLogPublisher.removeObserver(observer)
+
+    observer.assert_empty()
--- src/wormhole/test/test_eventual.py
+++ src/wormhole/test/test_eventual.py
@@ -13,7 +13,7 @@ class IntentionalError(Exception):
     pass
 
 
-def test_eventually():
+def test_eventually(observe_errors):
     c = Clock()
     eq = EventualQueue(c)
     c1 = mock.Mock()
@@ -34,110 +34,20 @@ def test_eventually():
     assert d3.result == "value"
 
 
+def test_error(observe_errors):
+    c = Clock()
+    eq = EventualQueue(c)
+    c1 = mock.Mock(side_effect=IntentionalError)
+    eq.eventually(c1, "arg1", "arg2", kwarg1="kw1")
+    assert c1.mock_calls == []
 
-class _LogObserver:
-    """
-    Observes the Twisted logs and catches any errors.
-
-    @ivar _errors: A C{list} of L{Failure} instances which were received as
-        error events from the Twisted logging system.
-
-    @ivar _added: A C{int} giving the number of times C{_add} has been called
-        less the number of times C{_remove} has been called; used to only add
-        this observer to the Twisted logging since once, regardless of the
-        number of calls to the add method.
-
-    @ivar _ignored: A C{list} of exception types which will not be recorded.
-    """
-
-    def __init__(self):
-        self._errors = []
-        self._added = 0
-        self._ignored = []
-
-    def _add(self):
-        from twisted.python import log
-        if self._added == 0:
-            log.addObserver(self.gotEvent)
-        self._added += 1
-
-    def _remove(self):
-        from twisted.python import log
-        self._added -= 1
-        if self._added == 0:
-            log.removeObserver(self.gotEvent)
-
-    def _ignoreErrors(self, *errorTypes):
-        """
-        Do not store any errors with any of the given types.
-        """
-        self._ignored.extend(errorTypes)
-
-    def _clearIgnores(self):
-        """
-        Stop ignoring any errors we might currently be ignoring.
-        """
-        self._ignored = []
-
-    def flushErrors(self, *errorTypes):
-        """
-        Flush errors from the list of caught errors. If no arguments are
-        specified, remove all errors. If arguments are specified, only remove
-        errors of those types from the stored list.
-        """
-        if errorTypes:
-            flushed = []
-            remainder = []
-            for f in self._errors:
-                if f.check(*errorTypes):
-                    flushed.append(f)
-                else:
-                    remainder.append(f)
-            self._errors = remainder
-        else:
-            flushed = self._errors
-            self._errors = []
-        return flushed
-
-    def getErrors(self):
-        """
-        Return a list of errors caught by this observer.
-        """
-        return self._errors
-
-    def gotEvent(self, event):
-        """
-        The actual observer method. Called whenever a message is logged.
-
-        @param event: A dictionary containing the log message. Actual
-        structure undocumented (see source for L{twisted.python.log}).
-        """
-        if event.get("isError", False) and "failure" in event:
-            f = event["failure"]
-            if len(self._ignored) == 0 or not f.check(*self._ignored):
-                self._errors.append(f)
-
-lo = _LogObserver()
-
-def test_error():
-    lo._add()
-    try:
-        c = Clock()
-        eq = EventualQueue(c)
-        c1 = mock.Mock(side_effect=IntentionalError)
-        eq.eventually(c1, "arg1", "arg2", kwarg1="kw1")
-        assert c1.mock_calls == []
-
-        eq.flush_sync()
-        assert c1.mock_calls == [mock.call("arg1", "arg2", kwarg1="kw1")]
-    finally:
-        lo._remove()
-
-    lo.flushErrors(IntentionalError)
+    eq.flush_sync()
+    assert c1.mock_calls == [mock.call("arg1", "arg2", kwarg1="kw1")]
+    observe_errors.flush(IntentionalError)
 
 
 @ensureDeferred
-async def test_flush(reactor):
+async def test_flush(reactor, observe_errors):
     eq = EventualQueue(reactor)
     d1 = eq.fire_eventually()
     d2 = Deferred()
